55f8ef44624999baa83a596e22f86fc815ce1f38
yanay <yannay@qq.com>
2020-06-17 12:06:18 +0800

    删除了不必要文件,只保留了接口和demo

--- src/test/java/com/demo/springboot/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/demo/springboot/helloworld/HelloworldApplicationTests.java
@@ -1,11 +1,7 @@
 package com.demo.springboot.helloworld;
 
-import com.demo.springboot.helloworld.common.domain.Admin;
-import com.demo.springboot.helloworld.common.domain.Book;
 import com.demo.springboot.helloworld.common.domain.Room;
 import com.demo.springboot.helloworld.common.domain.Trade;
-import com.demo.springboot.helloworld.mapper.AdminMapper;
-import com.demo.springboot.helloworld.mapper.BookMapper;
 import com.demo.springboot.helloworld.mapper.RoomMapper;
 import com.demo.springboot.helloworld.mapper.TradeMapper;
 import com.demo.springboot.helloworld.service.MailService;
@@ -21,8 +17,7 @@ import java.util.Date;
 
 @SpringBootTest
 class HelloworldApplicationTests {
-    @Autowired
-    BookMapper bookMapper;
+
 
     @Autowired
     RoomMapper roomMapper;
@@ -35,30 +30,10 @@ class HelloworldApplicationTests {
     @Autowired
     RoomService roomService;
 
-    @Autowired
-    AdminMapper adminMapper;
 
     @Autowired
     private MailService mailService;
 
-    @Test
-    void contextLoads() {
-        for (int i = 1; i <= 103; i++) {
-            Book book = new Book();
-            book.setAuthor("作者-"+i);
-            book.setName("名字-"+i);
-            book.setPrice(Math.random()*100);
-            book.setRemark("备注信息-"+i);
-            bookMapper.insert(book);
-        }
-        for(int i=1;i<=2;i++)
-        {
-            Admin admin = new Admin();
-            admin.setLoginname("yanyan");
-            admin.setPassword("root");
-            adminMapper.insert(admin);
-        }
-    }
 
     @Test
     public void sendmail() {
