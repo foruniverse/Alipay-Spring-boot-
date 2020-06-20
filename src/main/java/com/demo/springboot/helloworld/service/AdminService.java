7b743b87c62935f1bc4ace5df10a640bd658d786
yanay <yannay@qq.com>
2020-06-20 20:30:46 +0800

    更新项目说明

--- src/main/java/com/demo/springboot/helloworld/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.demo.springboot.helloworld.service;
-
-import com.demo.springboot.helloworld.common.domain.Admin;
-
-public interface AdminService {
-    public Admin login(Admin admin);
-}
